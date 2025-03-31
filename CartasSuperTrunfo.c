0f4c14c53337955e7d00d55d069a087c2369a958
Gabriel Lima <glima9719@gmail.com>
2025-03-31 21:39:45 +0000

    Adição de comparação aos atributos das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <locale.h>
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
 // Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
@@ -11,6 +11,7 @@ int main() {
     char estado1[20],estado2[20], codigo_carta1[5],codigo_carta2[5], nome_cidade1[50], nome_cidade2[50];
     int populacao1, populacao2,pontos_turisticos1,pontos_turisticos2;
     float area1, pib1,area2, pib2;
+    setlocale(LC_ALL, "Portuguese");
    
     
 
@@ -67,7 +68,7 @@ int main() {
     float pib_percap1 = (float) pib1/populacao1;
   
     float inverso_densidade1 = 1.0/densidade_p1;
-    float super_poder1 = populacao1+pib1+area1+pontos_turisticos1+pib_percap1+inverso_densidade1;
+    float super_poder1 = (float) (populacao1+pib1+area1+pontos_turisticos1+pib_percap1+inverso_densidade1);
 
     
      float densidade_p2 = (float) populacao2/area2;
@@ -75,16 +76,62 @@ int main() {
     float pib_percap2 = (float) pib2/populacao2;
     
     float inverso_densidade2 = 1.0/densidade_p2;
-    float super_poder1 = populacao2+pib2+area2+pontos_turisticos2+pib_percap2+inverso_densidade2;
+    float super_poder2 = (float) (populacao2+pib2+area2+pontos_turisticos2+pib_percap2+inverso_densidade2);
 
-    int comparativo_pop;
+    int comparativo_pop= populacao1>populacao2;
 
     if (comparativo_pop==1){
-        printf("População: Carta 1 venceu (%d)", comparativo_pop);
+        printf("População: Carta 1 venceu (%d)\n", comparativo_pop);
     }else{
-        printf("População: Carta 2 venceu (%d)", comparativo_pop);
-        return 0;
+        printf("População: Carta 2 venceu (%d)\n", comparativo_pop);
+        
     }
+    int comparativo_area= area1>area2;
+    if (comparativo_area==1){
+        printf("Área: Carta 1 venceu (%d)\n", comparativo_area);
+    }else{
+        printf("Área: Carta 2 venceu (%d)\n", comparativo_area);
+        
+    }
+    int comparativo_pib= pib1>pib2;
+    if (comparativo_area==1){
+        printf("PIB: Carta 1 venceu (%d)\n", comparativo_pib);
+    }else{
+        printf("PIB: Carta 2 venceu (%d)\n", comparativo_pib);
+        
+    }
+    int comparativo_turi= pontos_turisticos1>pontos_turisticos2;
+    if (comparativo_turi==1){
+        printf("Pontos turísticos: Carta 1 venceu (%d)\n", comparativo_turi);
+    }else{
+        printf("Pontos turísticos: Carta 2 venceu (%d)\n", comparativo_turi);
+        
+    }
+    int comparativo_den= densidade_p1>densidade_p2;
+    if (comparativo_den==1){
+        printf("Densidade demográfica: Carta 2 venceu (%d)\n", comparativo_den);
+    }else{
+        printf("Densidade demográfica: Carta 1 venceu (%d)\n", comparativo_den);
+
+
+    }
+    int comparativo_ppc= pib_percap1>pib_percap2;
+    if (comparativo_ppc==1){
+        printf("PIB per Capta: Carta 1 venceu (%d)\n", comparativo_ppc);
+    }else{
+        printf("PIB per Capta: Carta 2 venceu (%d)\n", comparativo_ppc);
+        
+    }
+    int comparativo_poder= super_poder1>super_poder2;
+    if (comparativo_poder==1){
+        printf("Super Poder: Carta 1 venceu (%d)\n", comparativo_poder);
+    }else{
+        printf("Super Poder: Carta 2 venceu (%d)\n", comparativo_poder);
+        
+    }
+    
+    
+
 
 
   

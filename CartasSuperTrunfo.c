e91c6aab0137a95db1bc8d10113eae562971e8f5
Gabriel Lima <glima9719@gmail.com>
2025-05-15 21:41:32 +0000

    Adição do menu para escolha da comparação

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,7 @@ int main() {
     char estado1[20],estado2[20], codigo_carta1[5],codigo_carta2[5], nome_cidade1[50], nome_cidade2[50];
     int populacao1, populacao2,pontos_turisticos1,pontos_turisticos2;
     float area1, pib1,area2, pib2;
+    int opcao;
     setlocale(LC_ALL, "Portuguese");
    
     
@@ -61,74 +62,118 @@ int main() {
     
     printf("Qual a quantidade de pontos turisticos: \n");
     scanf("%i", &pontos_turisticos2);
-    
-    
-    float densidade_p1 = (float) populacao1/area1;
-    
-    float pib_percap1 = (float) pib1/populacao1;
-  
-    float inverso_densidade1 = 1.0/densidade_p1;
-    float super_poder1 = (float) (populacao1+pib1+area1+pontos_turisticos1+pib_percap1+inverso_densidade1);
 
-    
-     float densidade_p2 = (float) populacao2/area2;
-   
-    float pib_percap2 = (float) pib2/populacao2;
-    
-    float inverso_densidade2 = 1.0/densidade_p2;
-    float super_poder2 = (float) (populacao2+pib2+area2+pontos_turisticos2+pib_percap2+inverso_densidade2);
-
-    int comparativo_pop= populacao1>populacao2;
-
-    if (comparativo_pop==1){
-        printf("População: Carta 1 venceu (%d)\n", comparativo_pop);
-    }else{
-        printf("População: Carta 2 venceu (%d)\n", comparativo_pop);
+    printf("Agora escolha qual o atributo numérico deseja comparar:\n 1 - Populacao \n 2 - Area \n 3 - PIB \n 4 - Numero de pontos turisticos \n 5- Densidade demografica\n");
+     
+        scanf("%d", &opcao);
         
+        switch(opcao){
+            case 1: {
+            
+            printf("Entre os paises %s e %s, no quesito populacao o veredito e: ", &estado1, &estado2);
+                if (populacao1>populacao2){
+        printf("População: Carta 1 venceu \n");
+    }else if(populacao1==populacao2){
+        printf("Empate! Ambas cartas tem a mesma populacao");
+    }else{
+        printf("População: Carta 2 venceu \n" );
+        }
+        break;
+    
     }
-    int comparativo_area= area1>area2;
+ case 2: {
+        int comparativo_area= area1>area2;
+         printf("Entre os paises %s e %s, no quesito area o veredito e: ", &estado1, &estado2);
     if (comparativo_area==1){
         printf("Área: Carta 1 venceu (%d)\n", comparativo_area);
+    }else if(area1==area2){
+        printf("Empate! Ambas cartas tem a mesma area");
     }else{
         printf("Área: Carta 2 venceu (%d)\n", comparativo_area);
         
     }
-    int comparativo_pib= pib1>pib2;
-    if (comparativo_area==1){
+     break;
+}
+            
+            case 3 : {
+            int comparativo_pib= pib1>pib2;
+             printf("Entre os paises %s e %s, no quesito PIB o veredito e: ", &estado1, &estado2);
+    if (comparativo_pib==1){
         printf("PIB: Carta 1 venceu (%d)\n", comparativo_pib);
+    }else if(pib1==pib2){
+        printf("Empate! Ambas cartas tem o mesmo pib");
     }else{
         printf("PIB: Carta 2 venceu (%d)\n", comparativo_pib);
+    
         
     }
+     break;
+}
+
+case 4 : {
     int comparativo_turi= pontos_turisticos1>pontos_turisticos2;
+     printf("Entre os paises %s e %s, no quesito pontos turisticos o veredito e: ", &estado1, &estado2);
     if (comparativo_turi==1){
         printf("Pontos turísticos: Carta 1 venceu (%d)\n", comparativo_turi);
+    }else if(pontos_turisticos1==pontos_turisticos2){
+        printf("Empate! Ambas cartas tem a quantidade de pontos turísticos");
     }else{
         printf("Pontos turísticos: Carta 2 venceu (%d)\n", comparativo_turi);
         
     }
+     break;
+}
+case 5 :{
+    float densidade_p1 = (float) populacao1/area1;
+    float densidade_p2 = (float) populacao2/area2;
     int comparativo_den= densidade_p1>densidade_p2;
+     printf("Entre os paises %s e %s, no quesito densidade demografica o veredito e: ", &estado1, &estado2);
     if (comparativo_den==1){
         printf("Densidade demográfica: Carta 2 venceu (%d)\n", comparativo_den);
+    }else if(densidade_p1==densidade_p2){
+        printf("Empate! Ambas cartas tem a mesma densidade demografica");
     }else{
         printf("Densidade demográfica: Carta 1 venceu (%d)\n", comparativo_den);
 
+    }
+     break;
+}
+ }
+    
+    
+    
+    
+    //float pib_percap1 = (float) pib1/populacao1;
+  
+    //float inverso_densidade1 = 1.0/densidade_p1;
+    //float super_poder1 = (float) (populacao1+pib1+area1+pontos_turisticos1+pib_percap1+inverso_densidade1);
 
-    }
-    int comparativo_ppc= pib_percap1>pib_percap2;
-    if (comparativo_ppc==1){
-        printf("PIB per Capta: Carta 1 venceu (%d)\n", comparativo_ppc);
-    }else{
-        printf("PIB per Capta: Carta 2 venceu (%d)\n", comparativo_ppc);
+    
+     
+    //float pib_percap2 = (float) pib2/populacao2;
+    
+    //float inverso_densidade2 = 1.0/densidade_p2;
+   // float super_poder2 = (float) (populacao2+pib2+area2+pontos_turisticos2+pib_percap2+inverso_densidade2);
+
+    
+
+    
+    
+    
+    
+   
+
+    //int comparativo_ppc= pib_percap1>pib_percap2;
+   // if (comparativo_ppc==1){
+    //    printf("PIB per Capta: Carta 1 venceu (%d)\n", comparativo_ppc);
+   // }else{
+       // printf("PIB per Capta: Carta 2 venceu (%d)\n", comparativo_ppc);
         
-    }
-    int comparativo_poder= super_poder1>super_poder2;
-    if (comparativo_poder==1){
-        printf("Super Poder: Carta 1 venceu (%d)\n", comparativo_poder);
-    }else{
-        printf("Super Poder: Carta 2 venceu (%d)\n", comparativo_poder);
-        
-    }
+   // }
+   //int comparativo_poder= super_poder1>super_poder2;
+   // if (comparativo_poder==1){
+  //  }else{
+     //   printf("Super Poder: Carta 2 venceu (%d)\n", comparativo_poder);}
     
     
 
